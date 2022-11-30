3f2d32653ea98c0a31c218896fb997ef519cdd10
Louie Espinosa <louie.espinosa@proton.me>
2022-11-30 14:52:49 -0600

    added vanilla js classList to replace jQuery addClass commands for shake and animate from animate.css

--- fCC daily exs/fCC-jQuery.js
+++ fCC daily exs/fCC-jQuery.js
@@ -1,8 +1,21 @@
 "use strict";
 
-$(document).ready(function() {
-    $("button").addClass("animated");
-    $(".btn").addClass("shake");
-    $("#target1").addClass("btn-primary");
-
-});
+// $(document).ready(function() {
+//     $("button").addClass("blue");
+//     $(".btn").addClass("shake");
+//     $("button").last().addClass("blue");
+//
+// });
+let buttons = document.querySelectorAll("button");
+    for(let i = 0; i < buttons.length; i++) {
+        let button = buttons[i];
+        button.addEventListener("click", function(){
+            this.classList.toggle("animate");
+        })
+    }
+let wells = document.querySelectorAll(".well");
+    for (let well of wells) {
+        well.addEventListener("mouseenter", function () {
+            this.classList.toggle("shake");
+        })
+    }
